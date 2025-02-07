6ef5150ae836fd04356f12706cef8f187a98a2b4
hrithik18k <hrithikvasanthram@gmail.com>
2025-02-07 13:20:22 +0530

    milestone-11

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -17,4 +17,13 @@ const createProduct = async (req, res) => {
     }
 };
 
-module.exports = { createProduct };
+const getAllProducts = async (req,res) => {
+    try{
+        const Products = await Product.find();
+        res.status(200).json(Products);
+    }catch(error){
+        res.status(500).json({message:"Error fetching products", error: error.message})
+    }
+}
+
+module.exports = { createProduct , getAllProducts };
