d40777db8bb6b74c4d949b20b7b41c9544999f92
hrithik18k <hrithikvasanthram@gmail.com>
2025-02-14 11:17:18 +0530

    milestone-15

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, useLocation } from 'react-router-dom';
 import ProductForm from './homepage/productform';
 import { useEffect, useState } from 'react';
 import Home from './homepage/Home';
@@ -7,9 +7,13 @@ import axios from 'axios';
 import Login from './components/Login';
 import SignUp from './components/signUp';
 import MyProducts from './components/MyProducts';
+import { Link, useNavigate } from "react-router-dom";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const email = localStorage.getItem("email");
 
   const fetchProducts = async () => {
     try {
@@ -22,10 +26,37 @@ function App() {
 
   useEffect(() => {
     fetchProducts();
-  }, []); // Empty 
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    navigate('/login');
+  };
 
   return (
-    <BrowserRouter>
+    <div>
+      {location.pathname !== '/login' && location.pathname !== '/signup' && (
+        <nav className="navbar">
+          <div className="navbar-left">
+            <h1 className="web-name">War-Mart</h1>
+          </div>
+          <div className="navbar-right">
+            <Link to={'/'} className="nav-link">Home</Link>
+            {email ? (
+              <>
+                <Link to={'/create'} className="nav-link">Add Product</Link>
+                <Link to={'/my-products'} className="nav-link">My Products</Link>
+                <button onClick={handleLogout} className="nav-link">Logout</button>
+              </>
+            ) : (
+              <>
+                <Link to={'/login'} className="nav-link">Login</Link>
+                <Link to={'/signup'} className="nav-link">Signup</Link>
+              </>
+            )}
+          </div>
+        </nav>
+      )}
       <Routes>
         <Route path="/" element={<Home products={products} />} />
         <Route path="/create" element={<ProductForm setProducts={setProducts} />} />
@@ -34,7 +65,7 @@ function App() {
         <Route path="/my-products" element={<MyProducts products={products} />} />
         <Route path="/edit/:id" element={<ProductForm setProducts={setProducts} />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
