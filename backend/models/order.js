b28b36d65646bec9c8d350250d8e7009dca78c2c
hrithik18k <hrithikvasanthram@gmail.com>
2025-02-18 15:02:49 +0530

    milestone-23

---
+++ backend/models/order.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    orderItems: [
+        {
+            name: { type: String, required: true },
+            quantity: { type: Number, required: true },
+            image: { type: String, required: true },
+            price: { type: Number, required: true },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            }
+        }
+    ],
+    shippingAddress: {
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        country: { type: String, required: true }
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending'
+    },
+    deliveredAt: {
+        type: Date
+    }
+}, {
+    timestamps: true
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
