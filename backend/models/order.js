b1a10d80c3498b5ece842480fd07e4576e9ffa66
hrithik18k <hrithikvasanthram@gmail.com>
2025-02-19 23:25:55 +0530

    milestone-29

--- backend/models/order.js
+++ backend/models/order.js
@@ -36,4 +36,6 @@ const orderSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Order', orderSchema);
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
