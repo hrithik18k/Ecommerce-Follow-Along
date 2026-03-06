939ae461af69d3f8f5985381b45fe7d0f1194c91
hrithik18k <hrithikvasanthram@gmail.com>
2026-03-06 10:59:52 +0530

    Configure app for Render deployment with specific frontend CORS and global backend URL environment variables

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    backendUrl: import.meta.env.VITE_BACKEND_URL || "${import.meta.env.VITE_BACKEND_URL || `${import.meta.env.VITE_BACKEND_URL || `${import.meta.env.VITE_BACKEND_URL || "http://localhost:3001"}`}`}"
+    backendUrl: import.meta.env.VITE_BACKEND_URL || "${import.meta.env.VITE_BACKEND_URL || "https://ecommerce-follow-along-1-1fss.onrender.com"}`}"
 };
 
 export default config;
